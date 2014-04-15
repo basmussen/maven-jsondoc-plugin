d3f5cb050b57dfd4fe4c3c1785186627a3d0ce73
travis-ci <travis@travis-ci.org>
2014-04-15 19:45:24 +0200

    modify script

--- script/travis_artifact_deployment.sh
+++ script/travis_artifact_deployment.sh
@@ -6,11 +6,11 @@ echo -e "Current repo: $TRAVIS_REPO_SLUG\n"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
-git clone -q --branch=gh-pages https://${GH_TOKEN}@github.com/basmussen/maven-jsondoc-plugin.git gh-pages > /dev/null 2>&1 || error_exit "Error cloning repository";
+cd gh-pages
 
-#git add .
-#git commit -q -m "Travis build $TRAVIS_BUILD_NUMBER"
-#git push -fq origin gh-pages > /dev/null 2>&1 || error_exit "Error uploading artifacts"
+git add .
+git commit -q -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push -fq origin gh-pages > /dev/null 2>&1 || error_exit "Error uploading artifacts"
 
 # processing time
 end=$(date +%s)
