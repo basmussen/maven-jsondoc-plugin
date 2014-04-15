4e446154b36c33a69d3fdcac6bc89ff7bb1fbe95
travis-ci <travis@travis-ci.org>
2014-04-15 20:21:11 +0200

    revert

---
+++ script/travis_artifact_deployment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+start=$(date +%s)
+echo -e "Current repo: $TRAVIS_REPO_SLUG\n"
+
+# git
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+
+cd gh-pages
+
+git add .
+git commit -q -m "Travis build $TRAVIS_BUILD_NUMBER"
+git push -fq origin gh-pages > /dev/null 2>&1 || error_exit "Error uploading artifacts"
+
+# processing time
+end=$(date +%s)
+elapsed=$(( $end - $start ))
+minutes=$(( $elapsed / 60 ))
+seconds=$(( $elapsed % 60 ))
+echo "Post-Build process finished in $minutes minute(s) and $seconds seconds"
